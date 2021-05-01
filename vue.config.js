3a34257a64505890785d3d6022239c01a0a41894
dmitry.nasyrkulov@gmail.com <dmitry.nasyrkulov@gmail.com>
2021-05-01 18:36:25 +0300

    some fixes

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,6 @@ module.exports = {
     disableHostCheck: true
   },
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
-  // assetsDir: 'assets',
   productionSourceMap: false,
   filenameHashing: false
 }
